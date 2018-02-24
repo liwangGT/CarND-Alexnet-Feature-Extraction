9d01407afaa8052558440723a663fc9325f374c5
liwangGT <wangliinhust@gmail.com>
2018-02-24 00:05:28 -0500

    updated feature extraction output layer

--- feature_extraction.py
+++ feature_extraction.py
@@ -19,7 +19,10 @@ fc7 = AlexNet(resized, feature_extract=True)
 # HINT: Look at the final layer definition in alexnet.py to get an idea of what this
 # should look like.
 shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
-probs = ...
+weight = {'wf': tf.Variable(tf.truncated_normal(shape=shape, mean =0, stddev=0.1))}
+biases = {'bf': tf.Variable(tf.zeros(nb_classes))}
+logits = tf.add(tf.matmul(fc7, weight['wf']), biases['bf'])
+probs = tf.nn.softmax(logits)
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
